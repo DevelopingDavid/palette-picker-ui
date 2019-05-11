1c8b82910f772048cbf76b2a2b2af09960df2a32
David Cisneros <Dcisneros101@gmail.com>
2019-05-11 12:07:26 -0600

    Add intial setup for project
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>
    Co-authored-by: Eric Fitzsimons <ericfitzsimons451@gmail.com>

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className='card'>
+      <i className="material-icons">lock_open</i>
+      <p>{}</p>
+    </div>
+  )
+}
+
+export default Card;
