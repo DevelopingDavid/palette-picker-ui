5a9496225710e26b085f7b5313e34ee146cc61cd
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-11 13:56:07 -0600

    Add lock/unlock functionality to cards
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>
    Co-authored-by: David Cisneros <dcisneros101@gmail.com>

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = (props) => {
-  console.log("props in CARD",props)
-  
-  return (
-    <div className='card' style={{backgroundColor: props.color}}>
-      <i className="material-icons lock-open">lock_open</i>
-      <p>{props.color.color}</p>
-    </div>
-  )
+class Card extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      locked: false
+    }
+  }
+
+  toggleLock = () => {
+    this.setState({
+      locked: !this.state.locked
+    })
+  }
+
+  render() {
+    
+    return (
+      <div className='card' style={{backgroundColor: this.props.color}} onClick={this.toggleLock}>
+      {
+        this.state.locked ? <i className="material-icons lock-closed">lock</i> : <i className="material-icons lock-open">lock_open</i>
+      } 
+        <p>{this.props.color.color}</p>
+      </div>
+    )
+  }
 }
 
 export default Card;
