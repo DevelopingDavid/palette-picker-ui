34fa179f5ca10e314937163de9d9bfc3864468db
David Cisneros <Dcisneros101@gmail.com>
2019-05-11 11:27:41 -0600

    Initialize repo
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>
    Co-authored-by: Eric Fitzsimons <ericfitzsimons451@gmail.com>

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderBar from '../HeaderBar/HeaderBar';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="app">
+        <HeaderBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
