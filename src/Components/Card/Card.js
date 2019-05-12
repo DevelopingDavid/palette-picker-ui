2885c73b93785685224e043b26c2b3e59134a5f5
Justin Torrance <justin.torrance91@gmail.com>
2019-05-12 12:14:53 -0600

    Remove console logs

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -12,7 +12,6 @@ class Card extends Component {
 
   toggleLock = () => {
     const hex = this.props.colorObject.hex
-    // console.log('hex', hex)
     this.props.toggleLockInStore(hex)
     this.toggleLockInState()
   }
@@ -28,7 +27,6 @@ class Card extends Component {
     
     return (
       <div className='card' 
-        // onClick={(hex) => this.props.toggleLockInStore(hex)}
         onClick={this.toggleLock} 
         style={{backgroundColor: hex}} 
         >
--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -6,7 +6,6 @@ import { connect } from 'react-redux'
 class CardContainer extends Component {
 
  render() {
-  console.log('props', this.props)
   const { currentPalette } = this.props
   const makeCards = currentPalette.map(color => {
       return <Card colorObject={color} />
